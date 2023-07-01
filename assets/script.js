194d100d524c29c0aa40780277896839f0acab5a
TheLastVelo <jarellchinn44@gmail.com>
2023-06-30 21:53:08 -0700

    added all confirms

--- assets/script.js
+++ assets/script.js
@@ -22,26 +22,44 @@ function generatePassword() {
   var lengthofP = window.prompt(
     "How many characters would you like your password (Must be larger than 8)"
   );
-  if (lengthofP < 8 || lengthofP > 128) {
-    alert("Must be between 8 and 128");
+  if (lengthofP < 8 || lengthofP > 128 || isNaN(lengthofP)) {
+    alert("Must be a number between 8 and 128");
     return "";
   }
   // ask if they what upper case
+  var upperCaseask = confirm("Would you like uppercase letters?");
   // confirm
   // if user confirmed add upper case letters to available char
+  if (upperCaseask) {
+    availableChar.push(upperCase);
+  }
   // ask if they what lowercase
   // confirm
+  var lowerCaseAsk = confirm("Would you like lowercase letters");
   // if user confirmed add lowercase letters to available char
+  if (lowerCaseAsk) {
+    availableChar.push(lowerCase);
+  }
   // ask if they what special characters
   // confirm
+  var specialCharAsk = confirm("Would you like special characters");
   // if user confirmed add special char to available char
+  if (specialCharAsk) {
+    availableChar.push(specialChar);
+  }
   // ask if they what numbers
   // confirm
+  var numbersAsk = confirm("Would you like numbers");
   // if user confirmed add numbers to available char
+  if (numbersAsk) {
+    availableChar.push(numbersOptions);
+  }
 
   // phase 2 building the password
   // create password variable and initialize as empty string
+
   // for loop that runs length times
+
   // create a random number between 0-length of available char array
   // select and element from the array at the index of the random number
   // add that char to end of password
